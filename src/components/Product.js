983a965a2e001c5834864ef9d459c9c8c89e3268
NipuNVemula06 <nipunvemula4@gmail.com>
2022-01-01 19:10:16 +0530

    link to route

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,13 +4,14 @@ import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
 import { useDispatch } from "react-redux";
 import { addToBasket } from "../slices/basketSlice";
-import Link from "next/Link";
+import { useRouter } from "next/router";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
   const dispatch = useDispatch();
+  const router = useRouter();
 
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
@@ -36,19 +37,20 @@ function Product({ id, title, price, description, category, image }) {
 
   return (
     <>
-      <div className="relative flex flex-col m-5 bg-white z-30 p-10 shadow-md hover:shadow-xl">
+      <div
+        onClick={() => router.push(`/product/${id}`)}
+        className="relative flex flex-col m-5 bg-white z-30 p-10 shadow-md hover:shadow-xl cursor-pointer"
+      >
         <p className="absolute top-2 right-2 text-xs italic text-gray-400">
           {category}
         </p>
-        <Link href={"/product/" + id}>
-          <Image
-            className="cursor-pointer"
-            src={image}
-            height={200}
-            width={200}
-            objectFit="contain"
-          />
-        </Link>
+        <Image
+          className="cursor-pointer"
+          src={image}
+          height={200}
+          width={200}
+          objectFit="contain"
+        />
 
         <h4 className="my-3">{title}</h4>
 
